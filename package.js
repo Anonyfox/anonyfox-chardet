33a77ca6261e6f139b53e2e2ebaf3e251492bc67
Maximilian Stroh <Hisako1337@gmail.com>
2014-12-21 13:09:34 +0100

    bugfix

--- package.js
+++ package.js
@@ -2,14 +2,14 @@ Package.describe({
   name: 'anonyfox:chardet',
   summary: 'Wrapper for https://github.com/runk/node-chardet',
   author: 'Maximilian Stroh',
-  version: '0.0.1',
+  version: '0.0.3',
   git: 'https://github.com/Anonyfox/anonyfox-chardet.git'
 });
 
 Npm.depends({chardet: '0.0.8'})
 
 Package.onUse(function(api) {
-  api.versionsFrom('0.9.0');
-  api.addFiles('anonyfox:chardet.js');
+  api.versionsFrom('1.0');
+  api.addFiles('chardet.js','server');
   api.export('chardet','server');
 });
